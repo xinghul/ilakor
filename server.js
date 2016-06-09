5d400f07d1666e779a9fa963b8e3db8eef885c20
Levi Lu <xinghu1989@gmail.com>
2016-06-09 15:50:18 -0700

    use same error handlers for dev and prod

--- server.js
+++ server.js
@@ -113,32 +113,18 @@ app.use(function(req, res, next) {
 
 /// error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get("env") === "development") {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    
-    // send back the json for now
-    // because we're not using jade as view engine
-    // so render with letiable injection does not work
-    res.json(err);
-    
-    // res.render("error", {
-    //   message: err.message,
-    //   error: err
-    // });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render("error", {
-    message: err.message,
-    error: {}
-  });
+  
+  // send back the json for now
+  // because we're not using jade as view engine
+  // so render with letiable injection does not work
+  res.json(err);
+  
+  // res.render("error", {
+  //   message: err.message,
+  //   error: err
+  // });
 });
 
 //exports for future use
